912451fd5e8f76bcdcfa4c841be2def4fecd37e7
Sofiya Ridnaya <ridnaya.sofiya@gmail.com>
2025-02-06 15:32:13 +0300

    add get data function

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from typing import Union
 from src.masks import get_mask_card_number, get_mask_account
+from datetime import datetime
 
 
 def mask_account_card(full_bank_data: str) -> str:
@@ -17,13 +18,22 @@ def mask_account_card(full_bank_data: str) -> str:
         return " ".join(first_word[:-1]) + f" {masked_number}"
 
 
-if __name__ == "__main__":
-    test_data = ["Visa Platinum 8990922113665229",
-"Visa Gold 5999414228426353",
-"Счет 73654108430135874305"]
-    for data in test_data:
-        print(mask_account_card(data))
+def get_date(date_string: str) -> str:
+    '''Функция принимает строку с датой и возвращает строку в формате ДД.ММ.ГГГГ'''
+    dt = datetime.fromisoformat(date_string)
+    # преобразует строку в объект datetime
+    return dt.strftime("%d.%m.%Y")
+    # форматирует в ДД.ММ.ГГГГ
 
 
+# проверка работы функций
+# if __name__ == "__main__":
+#     test_data = ["Visa Platinum 8990922113665229",
+# "Visa Gold 5999414228426353",
+# "Счет 73654108430135874305"]
+#     for data in test_data:
+#         print(mask_account_card(data))
+#
+# print(get_date("2024-03-11T02:26:18.671407"))
 
 
